7d0f7931e1cf0ad3b20b7956a52285a5876e8671
Jacques Tardie <hi@jacquestardie.org>
2016-08-17 16:31:52 -0700

    inherit from alpha_mask

--- rio_alpha/islossy.py
+++ rio_alpha/islossy.py
@@ -1,27 +1,7 @@
 import numpy as np
 from skimage import measure
 
-
-def _mask_exact(img, ndv):
-    '''Exact nodata masking based on ndv
-
-    Parameters
-    -----------
-    img: ndarray
-        (depth x rows x cols) array
-    ndv: (list|tuple|ndarray)
-        list of notdata values where len == img depth
-
-    Returns
-    --------
-    alpha: ndarray
-        ndarray mask of shape (rows, cols) where opaque == 0 and transparent == max of dtype
-    '''
-    depth, _, _ = img.shape
-    nd = np.iinfo(img.dtype).max
-    alpha = np.invert(np.all(np.rollaxis(img, 0, depth) == (ndv), axis=2)).astype(img.dtype) * nd
-
-    return alpha
+from rio_alpha.alpha_mask import mask_exact
 
 
 def count_ndv_regions(ndv, img):
@@ -41,7 +21,7 @@ def count_ndv_regions(ndv, img):
     '''
     np.set_printoptions(threshold=np.nan)
 
-    img = _mask_exact(ndv, img)
+    img = mask_exact(ndv, img)
 
     _, n_labels = measure.label(
         img,
