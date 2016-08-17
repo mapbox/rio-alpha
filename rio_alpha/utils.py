66a4f491f369075762f93dbdc7dd16566666ea62
virginiayung <virginia@mapbox.com>
2016-08-17 15:04:11 -0700

    Add docstrings to utils

--- rio_alpha/utils.py
+++ rio_alpha/utils.py
@@ -3,6 +3,17 @@ import json
 
 
 def _parse_single(n):
+    """Returns a single value nodata of type float
+
+    Parameters
+    ----------
+    n: integer or str(integer)
+
+    Returns
+    -------
+    float(n)
+
+    """
     try:
         return float(n)
     except ValueError:
@@ -10,12 +21,26 @@ def _parse_single(n):
 
 
 def _parse_ndv(ndv, bands):
+    """Returns a list of nodata values of type float
+
+    Parameters
+    ----------
+    ndv: string, str(list of nodata values)
+    bands: integer, band count
+
+    Returns
+    -------
+    list: list of floats, length = band count
+
+    """
+
     if re.match(r'\[[0-9\.\,\s]+\]', ndv):
         ndvals = [_parse_single(n) for n in json.loads(ndv)]
         if len(ndvals) != bands:
-            raise ValueError('{0} parsed to ndv of {1} does not match band count of {2}'.format(
-                ndv, json.dumps(ndvals), bands
-                ))
+            raise ValueError('{0} parsed to ndv of {1} does '
+                             'not match band count of {2}'.format(
+                              ndv, json.dumps(ndvals), bands
+                             ))
         else:
             return ndvals
     else:
