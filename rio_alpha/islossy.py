3ac45e5cf47b684d79fd74a9df296542321d8d90
Jacques Tardie <hi@jacquestardie.org>
2016-08-16 22:52:10 -0700

    modularize islossy function

--- rio_alpha/islossy.py
+++ rio_alpha/islossy.py
@@ -1,7 +1,11 @@
-import click
 import numpy as np
-import skimage
 from skimage import measure
+
+
+def _label(ndv, rgb):
+    """
+    """
+    return np.invert(np.all(np.rollaxis(rgb, 0, 3) == (ndv), axis=2)).astype(rgb.dtype) * np.iinfo(rgb.dtype).max
 
 
 def count_ndv_regions(ndv, rgb):
@@ -10,7 +14,13 @@ def count_ndv_regions(ndv, rgb):
     """
     np.set_printoptions(threshold=np.nan)
 
-    label_img = np.invert(np.all(np.rollaxis(rgb, 0, 3) == (ndv), axis=2)).astype(rgb.dtype) * np.iinfo(rgb.dtype).max
-    label_array, n_labels = skimage.measure.label(label_img, background=255, neighbors=4, return_num=True)
+    img = _label(ndv, rgb)
+
+    _, n_labels = measure.label(
+        img,
+        background=255,
+        neighbors=4,
+        return_num=True
+    )
 
     return n_labels
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,25 +5,25 @@ from click.testing import CliRunner
 from rio_alpha.scripts.cli import islossy
 
 
-def test_cli_expected_failures():
-    runner = CliRunner()
-
-    result = runner.invoke(islossy, [
+def test_cli_usage():
+    result = CliRunner().invoke(islossy, [
         'tests/fixtures/dne.tif'
     ])
     assert result.exit_code == 2
     assert 'Invalid value for "input"' in result.output
 
 
-def test_cli_expected_successes():
-    runner = CliRunner()
-
-    result = runner.invoke(islossy, [
-        'tests/fixtures/ca_chilliwack/2012_30cm_592_5452.tiny.tif',
-        '--ndv', '[255, 255, 255]'
+def test_lossy_single_value_ndv():
+    result = CliRunner().invoke(islossy, [
+        'tests/fixtures/ca_chilliwack/2012_30cm_594_5450.tiny.tif',
+        '--ndv', '255'
     ])
     assert result.exit_code == 0
-    assert result.output.strip('\n') == ""
+    assert result.output.strip('\n') == "--lossy lossy"
+
+
+def test_lossy_three_value_ndv():
+    runner = CliRunner()
 
     result = runner.invoke(islossy, [
         'tests/fixtures/ca_chilliwack/2012_30cm_594_5450.tiny.tif',
@@ -31,34 +31,6 @@ def test_cli_expected_successes():
     ])
     assert result.exit_code == 0
     assert result.output.strip('\n') == "--lossy lossy"
-
-    result = runner.invoke(islossy, [
-        'tests/fixtures/ca_chilliwack/2012_30cm_594_5452.tiny.tif',
-        '--ndv', '[255, 255, 255]'
-    ])
-    assert result.exit_code == 0
-    assert result.output.strip('\n') == "--lossy lossy"
-
-    result = runner.invoke(islossy, [
-        'tests/fixtures/ca_chilliwack/2012_30cm_594_5454.tiny.tif',
-        '--ndv', '[255, 255, 255]'
-    ])
-    assert result.exit_code == 0
-    assert result.output.strip('\n') == "--lossy lossy"
-
-    result = runner.invoke(islossy, [
-        'tests/fixtures/ca_kamloops/2012_6156D.tiny.tif',
-        '--ndv', '[255, 255, 255]'
-    ])
-    assert result.exit_code == 0
-    assert result.output.strip('\n') == "--lossy lossy"
-
-    result = runner.invoke(islossy, [
-        'tests/fixtures/dk_all/320_ECW_UTM32-EUREF89.tiny.tif',
-        '--ndv', '[18, 51, 62]'
-    ])
-    assert result.exit_code == 0
-    assert result.output.strip('\n') == ""
 
     result = runner.invoke(islossy, [
         'tests/fixtures/dk_all/450_ECW_UTM32-EUREF89.tiny.tif',
@@ -75,3 +47,28 @@ def test_cli_expected_successes():
     assert result.output.strip('\n') == "--lossy lossy"
 
 
+def test_notlossy_single_value_ndv():
+    result = CliRunner().invoke(islossy, [
+        'tests/fixtures/ca_chilliwack/2012_30cm_592_5452.tiny.tif',
+        '--ndv', '255'
+    ])
+    assert result.exit_code == 0
+    assert result.output.strip('\n') == ""
+
+
+def test_notlossy_three_value_ndv():
+    runner = CliRunner()
+
+    result = runner.invoke(islossy, [
+        'tests/fixtures/dk_all/320_ECW_UTM32-EUREF89.tiny.tif',
+        '--ndv', '[18, 51, 62]'
+    ])
+    assert result.exit_code == 0
+    assert result.output.strip('\n') == ""
+
+    result = runner.invoke(islossy, [
+        'tests/fixtures/ca_chilliwack/2012_30cm_592_5452.tiny.tif',
+        '--ndv', '[255, 255, 255]'
+    ])
+    assert result.exit_code == 0
+    assert result.output.strip('\n') == ""
