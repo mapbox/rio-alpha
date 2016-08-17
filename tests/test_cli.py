69b6ac1a8ee61f1322baca05d9701a1e2c2bf370
Jacques Tardie <hi@jacquestardie.org>
2016-08-17 11:34:37 -0700

    wip: improve cli_test structure
    per @virginayung suggestion

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,49 +5,28 @@ from click.testing import CliRunner
 from rio_alpha.scripts.cli import islossy
 
 
-def test_cli_expected_failures():
-    runner = CliRunner()
-
-    result = runner.invoke(islossy, [
+def test_cli_usage():
+    result = CliRunner().invoke(islossy, [
         'tests/fixtures/dne.tif'
     ])
     assert result.exit_code == 2
     assert 'Invalid value for "input"' in result.output
 
 
-def test_cli_lossy():
-    runner = CliRunner()
-
-    result = runner.invoke(islossy, [
+def test_lossy_single_value_ndv():
+    result = CliRunner().invoke(islossy, [
         'tests/fixtures/ca_chilliwack/2012_30cm_594_5450.tiny.tif',
         '--ndv', '255'
     ])
     assert result.exit_code == 0
     assert result.output.strip('\n') == "--lossy lossy"
 
+
+def test_lossy_three_value_ndv():
+    runner = CliRunner()
+
     result = runner.invoke(islossy, [
         'tests/fixtures/ca_chilliwack/2012_30cm_594_5450.tiny.tif',
-        '--ndv', '[255, 255, 255]'
-    ])
-    assert result.exit_code == 0
-    assert result.output.strip('\n') == "--lossy lossy"
-
-    result = runner.invoke(islossy, [
-        'tests/fixtures/ca_chilliwack/2012_30cm_594_5452.tiny.tif',
-        '--ndv', '[255, 255, 255]'
-    ])
-    assert result.exit_code == 0
-    assert result.output.strip('\n') == "--lossy lossy"
-
-    result = runner.invoke(islossy, [
-        'tests/fixtures/ca_chilliwack/2012_30cm_594_5454.tiny.tif',
-        '--ndv', '[255, 255, 255]'
-    ])
-    assert result.exit_code == 0
-    assert result.output.strip('\n') == "--lossy lossy"
-
-    result = runner.invoke(islossy, [
-        'tests/fixtures/ca_kamloops/2012_6156D.tiny.tif',
         '--ndv', '[255, 255, 255]'
     ])
     assert result.exit_code == 0
@@ -68,19 +47,28 @@ def test_cli_lossy():
     assert result.output.strip('\n') == "--lossy lossy"
 
 
-def test_cli_not_lossy():
+def test_notlossy_single_value_ndv():
+    result = CliRunner().invoke(islossy, [
+        'tests/fixtures/ca_chilliwack/2012_30cm_592_5452.tiny.tif',
+        '--ndv', '255'
+    ])
+    assert result.exit_code == 0
+    assert result.output.strip('\n') == ""
+
+
+def test_notlossy_three_value_ndv():
     runner = CliRunner()
 
     result = runner.invoke(islossy, [
-        'tests/fixtures/ca_chilliwack/2012_30cm_592_5452.tiny.tif',
-        '--ndv', '[255, 255, 255]'
+        'tests/fixtures/dk_all/320_ECW_UTM32-EUREF89.tiny.tif',
+        '--ndv', '[18, 51, 62]'
     ])
     assert result.exit_code == 0
     assert result.output.strip('\n') == ""
 
     result = runner.invoke(islossy, [
-        'tests/fixtures/dk_all/320_ECW_UTM32-EUREF89.tiny.tif',
-        '--ndv', '[18, 51, 62]'
+        'tests/fixtures/ca_chilliwack/2012_30cm_592_5452.tiny.tif',
+        '--ndv', '[255, 255, 255]'
     ])
     assert result.exit_code == 0
     assert result.output.strip('\n') == ""
