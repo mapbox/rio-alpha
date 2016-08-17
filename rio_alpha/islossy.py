a8c4ac243f97b20a707ea1d7efec0201d9169e9b
Jacques Tardie <hi@jacquestardie.org>
2016-08-16 22:52:10 -0700

    wip: modularize islossy function a bit

--- rio_alpha/islossy.py
+++ rio_alpha/islossy.py
@@ -4,13 +4,25 @@ import skimage
 from skimage import measure
 
 
+def _label(ndv, rgb):
+    """
+    """
+    return np.invert(np.all(np.rollaxis(rgb, 0, 3) == (ndv), axis=2)).astype(rgb.dtype) * np.iinfo(rgb.dtype).max
+
+
 def count_ndv_regions(ndv, rgb):
     """
     Discover unique labels to count ndv regions.
     """
     np.set_printoptions(threshold=np.nan)
 
-    label_img = np.invert(np.all(np.rollaxis(rgb, 0, 3) == (ndv), axis=2)).astype(rgb.dtype) * np.iinfo(rgb.dtype).max
-    label_array, n_labels = skimage.measure.label(label_img, background=255, neighbors=4, return_num=True)
+    img = _label(ndv, rgb)
+
+    _, n_labels = skimage.measure.label(
+        img,
+        background=255,
+        neighbors=4,
+        return_num=True
+    )
 
     return n_labels
