939c0e58c475e5be0aee4356572bc8f4f46831e8
virginiayung <virginia@mapbox.com>
2016-08-17 16:26:35 -0700

    Add findnodata cli, python api

--- rio_alpha/scripts/cli.py
+++ rio_alpha/scripts/cli.py
@@ -1,14 +1,18 @@
+import logging
 import click
 import rasterio as rio
-
-from rio_alpha.islossy import count_ndv_regions
 from rio_alpha.utils import _parse_ndv
+from rio_alpha.islossy import count_ndv_regions
+from rio_alpha.findnodata import determine_nodata
+
+logger = logging.getLogger('rio_alpha')
 
 
 @click.group('alpha')
 def alpha():
-    '''Nodata utilities
-    '''
+    """Add alpha band to imagery based on nodata values,
+        Find nodata in input image.
+    """
     pass
 
 
@@ -33,4 +37,37 @@ def islossy(input, ndv):
         click.echo("")
 
 
+@click.command('findnodata')
+@click.argument('src_path', type=click.Path(exists=True))
+@click.option('--user_nodata', '-u', default=None,
+              help="User supplies the nodata value, "
+              "input a single value or a list of per-band values.")
+@click.option('--debug', help="Enables matplotlib & printing of figures")
+@click.option('--verbose', '-v',
+              help="Prints extra information, "
+              "like competing candidate values")
+@click.option('--discovery', default=None,
+              help="Prints extra information, "
+              "like competing candidate values")
+def findnodata(src_path, user_nodata, debug, verbose, discovery):
+    determine_nodata(src_path, user_nodata)
+
+# with rio.drivers():
+#   with rio.open(args.infile, "r") as src:
+#     count = src.count
+
+# if ( count == 4 ):
+#     print "alpha"
+# else:
+#     nodata = src.nodata
+#     if ( nodata == None ):
+#         if discovery:
+#             discover_ndv()
+#         else:
+#             print ""
+#     else:
+#         print str(int(nodata)) + " " + str(int(nodata)) + " " + str(int(nodata))
+
+
 alpha.add_command(islossy)
+alpha.add_command(findnodata)
