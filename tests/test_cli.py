d700f816ea137de9c30b1c3c42e7dc8e137717c6
Jacques Tardie <hi@jacquestardie.org>
2016-08-16 17:54:50 -0700

    wip: strip newlines from test output

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -16,56 +16,56 @@ def test_cli_expected_outputs():
 
     result = runner.invoke(islossy, [
         'tests/fixtures/ca_chilliwack/2012_30cm_592_5452.tiny.tif',
-        '--ndv', '[255, 255, 255]\n'
+        '--ndv', '[255, 255, 255]'
     ])
     assert result.exit_code == 0
-    assert result.output == "--lossy lossy\n"
+    assert result.output.strip('\n') == ""
 
     result = runner.invoke(islossy, [
         'tests/fixtures/ca_chilliwack/2012_30cm_594_5450.tiny.tif',
         '--ndv', '[255, 255, 255]'
     ])
     assert result.exit_code == 0
-    assert result.output == "--lossy lossy\n"
+    assert result.output.strip('\n') == "--lossy lossy"
 
     result = runner.invoke(islossy, [
         'tests/fixtures/ca_chilliwack/2012_30cm_594_5452.tiny.tif',
         '--ndv', '[255, 255, 255]'
     ])
     assert result.exit_code == 0
-    assert result.output == "--lossy lossy\n"
+    assert result.output.strip('\n') == "--lossy lossy"
 
     result = runner.invoke(islossy, [
         'tests/fixtures/ca_chilliwack/2012_30cm_594_5454.tiny.tif',
         '--ndv', '[255, 255, 255]'
     ])
     assert result.exit_code == 0
-    assert result.output == "--lossy lossy\n"
+    assert result.output.strip('\n') == "--lossy lossy"
 
     result = runner.invoke(islossy, [
         'tests/fixtures/ca_kamloops/2012_6156D.tiny.tif',
         '--ndv', '[255, 255, 255]'
     ])
     assert result.exit_code == 0
-    assert result.output == "--lossy lossy\n"
+    assert result.output.strip('\n') == "--lossy lossy"
 
     result = runner.invoke(islossy, [
         'tests/fixtures/dk_all/320_ECW_UTM32-EUREF89.tiny.tif',
-        '--ndv', '[255, 255, 255]'
+        '--ndv', '[18, 51, 62]'
     ])
     assert result.exit_code == 0
-    assert result.output == "--lossy lossy\n"
+    assert result.output.strip('\n') == ""
 
     result = runner.invoke(islossy, [
         'tests/fixtures/dk_all/450_ECW_UTM32-EUREF89.tiny.tif',
         '--ndv', '[255, 255, 255]'
     ])
     assert result.exit_code == 0
-    assert result.output == "--lossy lossy\n"
+    assert result.output.strip('\n') == "--lossy lossy"
 
     result = runner.invoke(islossy, [
         'tests/fixtures/fi_all/W4441A.tiny.tif',
         '--ndv', '[255, 255, 255]'
     ])
     assert result.exit_code == 0
-    assert result.output == "--lossy lossy\n"
+    assert result.output.strip('\n') == "--lossy lossy"
