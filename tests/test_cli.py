16858078f358b1083e70b3b9e10f7fb08e72e339
Sean Gillies <sean@mapbox.com>
2021-04-20 13:36:45 -0600

    Update builds and require hashes for dependencies

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -13,8 +13,8 @@ def test_cli_missing_input():
     result = CliRunner().invoke(islossy, ["tests/fixtures/dne.tif"])
     assert result.exit_code == 2
     assert (
-        'Invalid value for "INPUT"' in result.output
-        or 'Invalid value for "input"' in result.output
+        "Invalid value for 'INPUT'" in result.output
+        or "Invalid value for 'input'" in result.output
     )
 
 
